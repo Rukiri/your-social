07a8364a35e5e234dad6d9a4f748da4b4960db9d
Rukiri <esitter172@gmail.com>
2015-11-13 23:45:38 -0600

    .

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ActiveRecord::Base
+	has_many :forums, :dependent => :destroy
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,4 @@
 class Topic < ActiveRecord::Base
+	belongs_to :forum
+	has many :posts, :dependent => :destroy 
 end

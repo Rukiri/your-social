071fde12950f75d31b59f51d0acc0c693d90bc81
Rukiri <esitter172@gmail.com>
2015-11-17 11:39:25 -0600

    adding gems

--- app/models/forum_post.rb
+++
@@ -1,5 +0,0 @@
-class ForumPost < ActiveRecord::Base
-	belongs_to :forum_thread
-	belongs_to :user
-        paginates_per 5
-end
--- db/migrate/20151114073954_add_foreign_to_forums.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignToForums < ActiveRecord::Migration
-  def change
-    add_column :forums, :category_id, :integer, :auto_increment => false
-  end
-end
